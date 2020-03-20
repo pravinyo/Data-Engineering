3d7bc6f3ba6e19198a90f6abe32b804535945189
Pratik P. Watwani <pratikpwatwani@gmail.com>
2020-03-20 13:33:08 -0400

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3.6 ./src/border_analytics.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
+python36 ./src/border_analytics.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
