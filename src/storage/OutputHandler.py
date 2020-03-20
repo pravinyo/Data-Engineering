e97ee6f317647ed8d6d68a7369b597bc975c8f3d
Pratik P. Watwani <pratikpwatwani@gmail.com>
2020-03-20 14:43:01 -0400

    Update OutputHandler.py

--- src/storage/OutputHandler.py
+++ src/storage/OutputHandler.py
@@ -1,10 +1,17 @@
-import pandas as pd
+import csv
 
 class OutputHandler:
     def __init__(self, outputFilePath):
         self.outputFilePath = outputFilePath
         
     def save_to_csv(self,header,output):
+
+
+        with open(self.outputFilePath, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+            writer.writerows(output)
+'''
         save = []
         
         #Add header data in list
@@ -20,4 +27,8 @@ class OutputHandler:
         print("Path:"+self.outputFilePath)
         df.to_csv(self.outputFilePath, sep=',', header=None, index=False)
         
+        return "Saved! Successfully"
+'''
+        #save to csv
+        print("Path:"+self.outputFilePath)
         return "Saved! Successfully"
